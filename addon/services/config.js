9393963b5ead7c3dbde1d215d148619156c0a562
Garrett Murphey <gmurphey@trovedata.com>
2016-01-13 20:19:50 -0500

    Moving lookupFactory to its own computed property.

--- addon/services/config.js
+++ addon/services/config.js
@@ -3,11 +3,16 @@ import getOwner from 'ember-getowner-polyfill';
 
 const {
   Service,
+  computed,
   get
 } = Ember;
 
 export default Service.extend({
+  __config__: computed(function() {
+    return getOwner(this)._lookupFactory('config:environment');
+  }),
+
   unknownProperty(path) {
-    return get(getOwner(this)._lookupFactory('config:environment'), path);
+    return get(this, `__config__.${path}`);
   }
 });
