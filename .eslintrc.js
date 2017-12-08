6f24174aade9c9f52d6badf0c4c2f2c8efd9b1dd
Garrett Murphey <gmurphey@gmurphey.com>
2017-12-07 21:46:47 -0500

    Cleaning up lint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
-  },
   extends: [
     'eslint:recommended',
     'plugin:ember/recommended'
@@ -11,12 +6,7 @@ module.exports = {
   plugins: [
     'ember'
   ],
-  env: {
-    browser: true
-  },
   rules: {
-    'ember/alias-model-in-controller': 'off',
-    'ember/named-functions-in-promises': 'off',
-    'ember/no-old-shims': 'error'
+    'ember/use-ember-get-and-set': 'error'
   }
 };
